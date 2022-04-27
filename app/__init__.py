7322208fb633272d3d37e3734ae6e5643b4a1c80
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-27 07:59:40 +0300

    ft:display-function - creates a function that displays popular movies

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,10 @@ from app.config import DevConfig
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
-from app import views
-
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
-
 from app import views
+
+
--- app/views.py
+++ app/views.py
@@ -1,20 +1,28 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
-# @app.route('/')
-# def index():
+@app.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     message = "My favourite Reuby , Baby steps muhimu"
-#     return render_template('index.html', message = message)
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-@app.route('/movie/<int:movie_id>')
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+
+@app.route('/movie/<movie_id>')
 def movie(movie_id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    title = f'You are viewing {movie_id}'
+    return render_template('movie.html',title = title)
+
